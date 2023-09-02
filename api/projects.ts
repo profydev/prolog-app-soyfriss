09ea00bc3f12681a34261c22fd647e502db6928c
soyfriss <federico.rissone@gmail.com>
2023-09-02 17:05:52 -0300

    Fix incorrect status in project

--- api/projects.ts
+++ api/projects.ts
@@ -1,9 +1,24 @@
 import { axios } from "./axios";
-import type { Project } from "./projects.types";
+import { Project, ProjectStatus } from "./projects.types";
 
 const ENDPOINT = "/project";
 
 export async function getProjects() {
   const { data } = await axios.get<Project[]>(ENDPOINT);
-  return data;
+
+  const projects = data.map((prj) => {
+    // Modify status value from API: "error" to "critical", "info" to "stable", "warning" is correct
+    let status = prj.status;
+    if (!Object.keys(ProjectStatus).includes(prj.status)) {
+      if (prj.status.toString() === "error") status = ProjectStatus.critical;
+      if (prj.status.toString() === "info") status = ProjectStatus.stable;
+    }
+
+    return {
+      ...prj,
+      status,
+    };
+  });
+
+  return projects;
 }
