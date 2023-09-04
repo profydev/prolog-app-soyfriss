cd58d8eeab4ee00332cd0c90c3bbb92b4a1d6ec8
soyfriss <federico.rissone@gmail.com>
2023-09-04 16:02:20 -0300

    Add footer

---
+++ features/layout/footer/footer.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import styles from "./footer.module.scss";
+
+const menuItems = [
+  { text: "Docs", href: "#" },
+  { text: "API", href: "#" },
+  { text: "Help", href: "#" },
+  { text: "Community", href: "#" },
+];
+
+export function Footer() {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.container}>
+        <nav>
+          <ul className={styles.list}>
+            {menuItems.map((menuItem, index) => (
+              <li key={index}>
+                <Link href={menuItem.href} className={styles.anchor}>
+                  {menuItem.text}
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+        <div className={styles.logoContainer}>
+          <img src="/icons/logo-small.svg" alt="logo" />
+        </div>
+        <div className={styles.versionContainer}>Version: 14.5.1</div>
+      </div>
+    </footer>
+  );
+}
